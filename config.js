fd43e1056ac9f6c111a3f8046d1e977e83a74a10
Mauro <mauro.taliente3@gmail.com>
2018-08-21 00:31:30 -0300

    go!

--- config.js
+++ config.js
@@ -1,16 +1,8 @@
 
-// module.exports = {
-//   domain: 'http://localhost:',
-//   port: process.env.PORT || 3000,
-//   db: process.env.MONGODB || 'mongodb://localhost:27017/apiRestDb',
-//   SECRET_TOKEN: 'estaEsMiClaveDeTokens'
-// }
-
-
   const ok = {
     domain: 'http://localhost:',
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/apiRestDb',
+    db: process.env.MONGODB || 'mongodb://demoUser:pasword3@ds227352.mlab.com:27352/db-demo',
     SECRET_TOKEN: 'estaEsMiClaveDeTokens',
     path: '',
     cloudDomain:'https://res.cloudinary.com',
@@ -20,6 +12,21 @@
       api_secret: '2BYAZP3ZJ6wCBXBHnIndPrmzdYs'
     }
   }
+
+
+  // const ok = {
+  //   domain: 'http://localhost:',
+  //   port: process.env.PORT || 3000,
+  //   db: process.env.MONGODB || 'mongodb://localhost:27017/apiRestDb',
+  //   SECRET_TOKEN: 'estaEsMiClaveDeTokens',
+  //   path: '',
+  //   cloudDomain:'https://res.cloudinary.com',
+  //   cloudConfig: {
+  //     cloud_name: 'hjzfc79v8', 
+  //     api_key: '418297315424413', 
+  //     api_secret: '2BYAZP3ZJ6wCBXBHnIndPrmzdYs'
+  //   }
+  // }
 
   let path = ok.domain + ok.port + '/'
   ok.path = path
