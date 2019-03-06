c8edf89175b86e75de9a6321a8e4476cca466cd7
Mauro <mauro.taliente3@gmail.com>
2019-03-06 17:36:22 -0300

    vuelve mail

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports =  {
   port: process.env.PORT || 3000,
   db: process.env.MONGODB_URI,
   email: {
-    host: "smtp.comerdespierto.com",
+    host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
